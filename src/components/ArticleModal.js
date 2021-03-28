91888ef221bb272f081c0fb99e0c1f8c36610164
Bartosz Białecki <bartosz.bialecki@gmail.com>
2021-03-28 22:42:16 +0200

    Add ArticleModal component

---
+++ src/components/ArticleModal.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Modal } from "antd";
+import { useMapStore } from "../pages/map/store";
+
+export default function ArticleModal() {
+  const [{ modalVisible, currentArticle }, { setModalVisible }] = useMapStore();
+  const { url, title } = currentArticle;
+
+  function handleCancel() {
+    setModalVisible(false);
+  }
+
+  return (
+    <div>
+      <Modal
+        title={title}
+        visible={modalVisible}
+        footer={null}
+        onCancel={handleCancel}
+        bodyStyle={{ height: "80vh" }}
+      >
+        <iframe
+          src={url?.replace("wikipedia.org", "m.wikipedia.org")}
+          title={title}
+          width="100%"
+          height="100%"
+          style={{ border: "none" }}
+        />
+      </Modal>
+    </div>
+  );
+}
