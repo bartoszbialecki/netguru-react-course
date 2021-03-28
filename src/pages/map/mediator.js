85697a2cd3161ac1c6316b57cdf7b3162c42a642
Bartosz Białecki <bartosz.bialecki@gmail.com>
2021-03-28 22:49:25 +0200

    Use map mediator

--- src/pages/map/mediator.js
+++ src/pages/map/mediator.js
@@ -1,24 +1,101 @@
 import api from "../../services/api/wikipedia";
+import articlesDatabase from "../../services/articlesDatabase";
+import { useMapStore } from "./store";
+import debounce from "lodash/debounce";
 
 const listeners = {};
+let map;
+
+const defaultArticleColor = "orange";
+const readArticleColor = "blue";
+
+const callGetArticlesAfterLastInvocationMs = 1000;
 
 export function emit(event, ...args) {
-  listeners[event](...args);
+  const listener = listeners[event];
+
+  if (listener) {
+    listener(...args);
+  }
 }
 
 function attachListener(eventName, listener) {
   listeners[eventName] = listener;
 }
 
+function mapWikipediaArticlesToMarkers(articles) {
+  return articles.map(({ lat, lon, pageid, title }) => ({
+    pageid,
+    lat,
+    lng: lon,
+    title,
+  }));
+}
+
+function mapReadArticles(articles) {
+  return articles.map(({ title, ...rest }) => ({
+    ...rest,
+    title,
+    color: articlesDatabase.isArticleRead(title)
+      ? readArticleColor
+      : defaultArticleColor,
+  }));
+}
+
 function useMapMediator() {
-  async function mapDragged(center) {
-    // fetch articles
-    console.log("map dragged", center);
-    const articles = await api.getArticles({ coord: center });
-    console.log(articles);
+  const [
+    ,
+    {
+      addMarkers,
+      setGoogleApiLoaded,
+      setModalVisible,
+      setCurrentArticle,
+      setMarkerColor,
+    },
+  ] = useMapStore();
+
+  const debouncedGetArticles = debounce(
+    getArticlesForMapCenter,
+    callGetArticlesAfterLastInvocationMs
+  );
+
+  async function getArticlesForMapCenter() {
+    const response = await api.getArticles({
+      coord: map.center.toJSON(),
+      limit: 100,
+    });
+    let articles = mapWikipediaArticlesToMarkers(response.query.geosearch);
+    articles = mapReadArticles(articles);
+    addMarkers(articles);
   }
 
-  attachListener("mapDragged", mapDragged);
+  function mapLoaded(mapInstance) {
+    map = mapInstance;
+    setGoogleApiLoaded(true);
+
+    map.addListener("idle", debouncedGetArticles);
+  }
+
+  function searchBoxPlacesSelected(position) {
+    if (map) {
+      map.setCenter(position);
+    }
+  }
+
+  async function markerClicked(title) {
+    const { query } = await api.getArticle({ title });
+    const articles = Object.values(query.pages);
+    const article = articles[0];
+    setCurrentArticle({ url: article.fullurl, title });
+    setModalVisible(true);
+    setMarkerColor({ title, color: readArticleColor });
+
+    articlesDatabase.setArticleAsRead(title);
+  }
+
+  attachListener("mapLoaded", mapLoaded);
+  attachListener("searchBoxPlacesSelected", searchBoxPlacesSelected);
+  attachListener("markerClicked", markerClicked);
 }
 
 export default function MapMediator() {
